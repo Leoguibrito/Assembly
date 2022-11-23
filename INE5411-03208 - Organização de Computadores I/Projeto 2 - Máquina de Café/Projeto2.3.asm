cf2ffcb08a673a9a81d3cfb331969d2374f40bcb
Leonardo Brito <92063418+Leoguibrito@users.noreply.github.com>
2022-11-23 17:34:26 -0300

    Add files via upload

---
+++ INE5411-03208 - Organização de Computadores I/Projeto 2 - Máquina de Café/Projeto2.3.asm
@@ -0,0 +1,35 @@
+.data
+
+.text	
+Inicializa:
+	li $s0, 20		#Doses Café Puro
+	li $s1, 20		#Doses Leite
+	li $s2, 20		#Doses Chocolate
+	li $s3, 20 		#Doses Acucar
+
+.globl main
+main:		
+			
+Cafe:			
+CafePuro:				
+CafeComLeite:						
+Mochaccino:
+
+
+
+Tamanho:
+Grande:
+Pequeno:
+
+
+QuerAcucar:
+
+
+PrepararBebida:
+
+
+GerarCupom:
+
+
+SairContinuar:
+	
